6310a28ba5e27c0ba66b5eb1b198d16a37c9a13e
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-06 17:26:27 +0430

    Solve the required changes

--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,8 @@
+require_relative 'my_enumerable'
 class MyList
+  include MyEnumerable
+  attr_reader :list
+
   def initialize(*list)
     @list = list
   end
@@ -7,3 +11,16 @@ class MyList
     @list.each(&block)
   end
 end
+
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts(list.all? { |e| e < 5 }) # Should print: true
+puts(list.all? { |e| e > 5 }) # Should print: false
+
+# Test #any?
+puts(list.any? { |e| e == 2 }) # Should print: true
+puts(list.any? { |e| e == 5 }) # Should print: false8
+
+# Test #filter
+p(list.filter(&:even?)) # Should print: [2, 4]
