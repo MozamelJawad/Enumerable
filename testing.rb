c6c140e344427dc021c116eeb2484362c3d4ec3e
Young-Gaius <youngehu8@gmail.com>
2023-10-06 08:46:39 +0100

    input testing

---
+++ testing.rb
@@ -0,0 +1,20 @@
+require_relative 'my_list'
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+end
+
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+
+# Test #all?
+puts list.all? { |e| e < 5 }  # Should print: true
+puts list.all? { |e| e > 5 }  # Should print: false
+
+# Test #any?
+puts list.any? { |e| e == 2 }  # Should print: true
+puts list.any? { |e| e == 5 }  # Should print: false8
+
+# Test #filter
+p list.filter { |e| e.even? }  # Should print: [2, 4]
